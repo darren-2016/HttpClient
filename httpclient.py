3bbf243e2f7355b515ad96a46c641de731c8c42e
darren-2016 <darren.draper@gpsengine.net>
2016-10-14 13:49:36 +1000

    ReadSMS2 renamed to ReadSMS.  Prev ReadSMS renamed to ReadSMS_old(0

--- httpclient.py
+++ httpclient.py
@@ -125,37 +125,11 @@ def SendSMS(connection, userid, passwd, requesturl, message, phone, printlog):
     response = connection.getresponse()
     print "Response status/reason: " + str(response.status), response.reason
 
-####################
-# ReadSMS
-#
-def ReadSMS(connection, userid, passwd, requesturl):
-    auth = 'Basic ' + string.strip(base64.encodestring(userid + ':' + passwd))
-    print "Auth: " + auth
-
-    #params = urllib.urlencode({"" : ""})
-
-    connection.set_debuglevel(1)
-
-    headers = {"Content-type": "application/x-www-form-urlencoded",
-               "Authorization": auth}
-
-    connection.putrequest("GET", requesturl)
-    connection.putheader("Content-type", "application/x-www-form-urlencoded")
-    connection.putheader("Authorization", auth)
-    connection.endheaders()
-
-    #import time
-    #time.sleep(5)
-    # Read the response after the request has been sent
-    response = connection.getresponse()
-    response.read()
-    print "Response status/reason: " + str(response.status), response.reason
-
 
 ##################################################
 # ReadSMS action
 #
-def ReadSMS2(connection, userid, passwd, requesturl, printlog):
+def ReadSMS(connection, userid, passwd, requesturl, printlog):
     if printlog > 0:
         print "\n-----------------------------------------\n###READ SMS"
     
@@ -363,5 +337,32 @@ def WorkerPending2(connection, userid, passwd, requesturl, phone, sid, workerpas
 
     except requests.exceptions.RequestException:
         print('HTTP Request failed')
+
+
+####################
+# ReadSMS
+#
+def ReadSMS_old(connection, userid, passwd, requesturl):
+    auth = 'Basic ' + string.strip(base64.encodestring(userid + ':' + passwd))
+    print "Auth: " + auth
+
+    #params = urllib.urlencode({"" : ""})
+
+    connection.set_debuglevel(1)
+
+    headers = {"Content-type": "application/x-www-form-urlencoded",
+               "Authorization": auth}
+
+    connection.putrequest("GET", requesturl)
+    connection.putheader("Content-type", "application/x-www-form-urlencoded")
+    connection.putheader("Authorization", auth)
+    connection.endheaders()
+
+    #import time
+    #time.sleep(5)
+    # Read the response after the request has been sent
+    response = connection.getresponse()
+    response.read()
+    print "Response status/reason: " + str(response.status), response.reason
 
 
